33802f406a252428d3881dc0d2b62953620dd5bf
Anirudh-1149 <45738362+Anirudh-1149@users.noreply.github.com>
2019-11-04 12:52:32 +0530

    Add files via upload

--- linked_lists.cpp
+++ linked_lists.cpp
@@ -2,21 +2,19 @@
 using namespace std;
 
 struct coordinate{
-	int x,y;
+	int x;
 	struct coordinate* next;
 };
 
-void AddFirst(int a,int b,struct coordinate** headptr){
+void AddFirst(int a,struct coordinate** headptr){
 	if(*headptr == NULL){
 		*headptr = new coordinate();
 		(*headptr)->x = a;
-		(*headptr)->y = b;
 		(*headptr)->next = NULL;
 	}
 	else{
 		coordinate* temp = new coordinate();
 		temp->x =a;
-		temp->y =b;
 		temp->next = *headptr;
 		*headptr = temp;
 	}
@@ -39,12 +37,12 @@ void Del(int a,int b,struct coordinate** headptr){
 	coordinate* temp_ = NULL; 
 	int flag =0;
 	while(temp!=NULL){
-		if(temp->x==a&&temp->y==b&&temp_!=NULL){
+		if(temp->x==a&&temp_!=NULL){
 			temp_->next = temp->next;
 			free(temp);
 			flag =1;
 		}
-		else if(temp_ == NULL&&temp->x==a&&temp->y==b){
+		else if(temp_ == NULL&&temp->x==a){
 			DelFirst(headptr);
 			flag =1;
 		}
@@ -57,31 +55,11 @@ void Del(int a,int b,struct coordinate** headptr){
 			cout<<"-1"<<"\n";
 		}
 }
-void search(double d, struct coordinate** headptr){
-	coordinate* temp = *headptr;
-	int flag =0;
-	while(temp!= NULL){
-		double len = sqrt((temp->x)*(temp->x)+(temp->y)*(temp->y));
-		if(len<=d){
-			temp = temp->next;
-			flag++;
-		}
-		else{
-			temp = temp->next;	
-		}
-	}
-	if(flag ==0){
-			cout<<"-1"<<"\n";
-		}
-	else{
-		cout<<flag<<"\n";
-		}
-}
-void search(int a ,int b ,struct coordinate** headptr){
+void search(int a ,struct coordinate** headptr){
 	coordinate* temp = *headptr;
 	int flag = 0;
 	while(temp!= NULL){
-		if(temp->x==a&&temp->y==b){
+		if(temp->x==a){
 			flag =1;
 			break; 
 		}
@@ -96,15 +74,6 @@ void search(int a ,int b ,struct coordinate** headptr){
 		cout<<"False"<<"\n";
 	}
 }
-void length(struct coordinate** headptr){
-	coordinate* temp = *headptr;
-	int i=0;
-	while(temp!=NULL){
-		temp= temp->next;
-		i++;
-	}
-	cout<<i; 
-}
 
 int main(){
 	coordinate* head = NULL;
@@ -114,27 +83,23 @@ int main(){
 	while(T--){
 		cin>>condition;
 		if(condition==1){
-			int a,b;
-			cin>>a>>b;
-			AddFirst(a,b,&head);
+			int a;
+			cin>>a;
+			AddFirst(a,&head);
 		}
 		else if(condition ==2){
 			DelFirst(&head);
 		}
 		else if(condition == 3){
-			int a,b;
-			cin>>a>>b;
-			Del(a,b,&head);
+			int a;
+			cin>>a;
+			Del(a,&head);
 		}
 		else if(condition==4){
-			int a,b;
-			cin>>a>>b;
-			search(a,b,&head);
+			int a;
+			cin>>a;
+			search(a,&head);
 		}
-		else if(condition ==5){
-			length(&head);
-		}
-
 	}
 	return 0;
 }
