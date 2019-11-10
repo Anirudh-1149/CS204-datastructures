54d112facb45942129e84ecba5e82eae6db52f37
Anirudh-1149 <45738362+Anirudh-1149@users.noreply.github.com>
2019-11-10 23:28:20 +0530

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -3,80 +3,105 @@ using namespace std;
 #define ll long long 
 #define f(i, a, b) for(ll i=a;i<b;i++)
 #define fi(i, a, b) for(ll i=b-1;i>=a;i--)
+#define pb push_back
+#define VI vector<int>
+#define Vll vector<long long>
 
-class Stack
-{
-	int *arr;
-	int size;
-	int top=-1;
-	public:
-		Stack(int size);
-		void push(int num);
-		void pop();
-		void peek();
-		void display();
-
-	
+struct coordinate{
+	int x;
+	struct coordinate* next;
 };
-Stack::Stack(int size)
-{
-	this->size=size;
-	arr = new int[size];
-}
 
-void Stack::push(int num)
-{
-	if(top>=size-1)
-		cout<<"Stack Overflow\n";
-	else
-	{
-		top++;
-		arr[top]=num;
+void Push(int a,struct coordinate** headptr){
+	if(*headptr == NULL){
+		*headptr = new coordinate();
+		(*headptr)->x = a;
+		(*headptr)->next = NULL;
 	}
-}
-
-void Stack::peek()
-{
-	if(top>=0)
-		cout<<arr[top]<<"\n";
-	else
-		cout<<"Stack is empty\n";
-}
-void Stack::pop()
-{
-	if(top>=0)
-	{
-		//cout<<"Element Poped is "<<arr[top]<<"\n";
-		top--;
-	}
-	else
-	{
-		cout<<"Stack is Empty";
+	else{
+		coordinate* temp = new coordinate();
+		temp->x =a;
+		temp->next = NULL;
+		coordinate* temp1 = *headptr;
+		while(temp1->next!=NULL)
+			temp1 = temp1->next;
+		temp1->next = temp; 
 	}
 
 }
 
-void Stack::display()
+void Pop(struct coordinate** headptr)
 {
-	if(top>=0)
-	{
-		fi(i,0,top+1)
-		{
-			cout<<arr[top]<<" ";
+	coordinate* temp1 = *headptr;
+	coordinate* temp2 = NULL;
+	while(temp1!=NULL &&temp1->next!=NULL){
+		temp2 = temp1;
+		temp1 = temp1->next;
+	}
+	if(temp1==NULL)
+		cout<<"Stack is empty";
+	else{
+		cout<<temp1->x<<" is popped\n";
+		if(temp2==NULL){
+			free(temp1);
+			*headptr = NULL;
 		}
-		cout<<"\n";
+		else
+		{
+			temp2->next = NULL;
+			free(temp1);		
+		}
 	}
-	else
-		cout<<"Stack is Empty";
 }
 
+void Top(coordinate** headptr)
+{
+	coordinate* temp = *headptr;
+	while (temp!=NULL &&temp->next!=NULL)
+	{
+		temp = temp->next;
+	}
+	if(temp==NULL)
+	cout<<"Stack is empty";
+	else
+	cout<<temp->x<<"\n";	
+}
 
-
+void size(coordinate ** headptr)
+{
+	coordinate* temp = *headptr;
+	int i=0;
+	while (temp!=NULL)
+	{
+		temp = temp->next;
+		i++;
+	}
+	cout<<i<<"\n";
+}
 
 int main()
-{	
-	Stack s(5);
-	s.push(1);
-	s.push(2);
-	s.peek();
+{
+	coordinate* root = NULL;
+	int a=0;
+		while(a!=5){
+			cin>>a;
+			if(a==1)
+			{
+				int x;
+				cin>>x;
+				Push(x,&root);
+			}
+			if(a==2)
+			{
+				Pop(&root);
+			}
+			if(a==3)
+			{
+				Top(&root);
+			}
+			if(a==4)
+			{
+				size(&root);
+			}
+		}
 }
